b3bec3674243fb39eff8462f0eb261d57f57f317
dajing <wer321011@gmail.com>
2018-11-01 22:16:01 +0800

    tmp

--- geocoding.rb
+++ geocoding.rb
@@ -11,14 +11,12 @@ file = File.read('public/post_boxes.json')
 post_boxes = JSON.parse(file)
 post_boxes.each do |post_box|
   address = post_box["full_address"]
-  pry.binding
   address = CGI.escape  address.to_s
   url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{address}&key=AIzaSyANrwP97rB6-tun03pB8yY3NPd3di2BRTI"
   response = RestClient.get url, :user_agent => "myagent"
   response_json = JSON.parse(response)
   post_box["lat"] = response_json["results"][0]["geometry"]["location"]["lat"] 
   post_box["lng"] = response_json["results"][0]["geometry"]["location"]["lng"] 
-  pry.binding
 end
 
 File.open("public/post_boxes_with_latlng.json","w") do |f|
